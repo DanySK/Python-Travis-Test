3fd17701df5afa03e8300fc324214b05d13bc8cb
Danilo Pianini <danilo.pianini@unibo.it>
2018-06-04 18:17:29 +0200

    Update description

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="my_pkg",
+    name="a_chart",
     version="0.1.0",
     author="Danilo Pianini",
     author_email="danilo.pianini@unibo.it",
-    description="A small example package",
+    description="Creates a chart with a sinusoid",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/DanySK/example-project",
